f42687bde53d685f310ad16a1e1c62f74a53b94b
Codey <codeymcdaniel@gmail.com>
2019-03-29 10:01:28 -0600

    Updates

---
+++ precache-manifest.64b55743807155d0b4c78b157aaa3637.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "749c269a439f4a68e6db",
+    "url": "/clicky-game/static/css/main.91149147.chunk.css"
+  },
+  {
+    "revision": "749c269a439f4a68e6db",
+    "url": "/clicky-game/static/js/main.d808a5b4.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/clicky-game/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "eb3b274fd6d14bce95e667c4bd673cf7",
+    "url": "/clicky-game/index.html"
+  }
+];
